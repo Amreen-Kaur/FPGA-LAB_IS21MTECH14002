2c5e3ac73f8b4a6c750b77b1e1f613ea050a1ad1
Amreen-Kaur <97379008+Amreen-Kaur@users.noreply.github.com>
2022-02-22 11:53:45 +0530

    Update Assignment-3.asm

--- Assignment-3/Assignment-3.asm
+++ Assignment-3/Assignment-3.asm
@@ -1,17 +1,18 @@
-.nolist
+.nolist ; inculded libraries
 .include "/home/krunal/Projects/avr_assembly/m328pdef.inc" 
 .list
 
-ldi r16,low(RAMEND) ; RAMEND address 0x08ff
+ldi r16,low(RAMEND) ; RAMEND address 0x08ff ;defining stack,stack pointer and memory limits
 out SPL,r16 ; Stack Pointer Low SPL at i/o address 0x3d
 ldi r16,high(RAMEND)
 out SPH,r16
              
 
-ldi r16,0b00100000
+ldi r16,0b00100000 ;let’s consider the Arduino Uno with the ATmega 328p micro-controller. DDR(B/D/C), PORT(B/D/C), PIN(B/D/C) are called port registers. They control how a port/pin works, either as an input or an output.
+                    DDRB is the Data Direction register for port “B”. This means that if you set this register to 0xFF (by running DDRB |= 0xFF ), all ports or pins in the “B” I/O port act as outputs. If you set DDRB to 0x00 (it’s initialized to 0x00 by default), then ports or pins in the “B” I/O port act as inputs.
 out DDRB,r16
 
-ldi r16,0b00000000
+ldi r16,0b00000000 ;The DDRD register sets the direction of Port D. Each bit of the DDRD register sets the corresponding Port D pin to be either an input or an output. A 1 makes the corresponding pin an output, and a 0 makes the corresponding pin an input. 
 out DDRD,r16
 ldi r16, 0b00000101 ; the last 3 bits define the prescaler, 101 => division by 1024
 out TCCR0B, r16
